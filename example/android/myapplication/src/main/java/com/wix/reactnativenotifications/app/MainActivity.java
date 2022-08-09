fe26781b38b993e12cd7a01f3214001083def2ed
Vitor Barros <vitormonteirodebarros@gmail.com>
2022-08-09 00:11:05 -0400

    first commit

---
+++ example/android/myapplication/src/main/java/com/wix/reactnativenotifications/app/MainActivity.java
@@ -0,0 +1,10 @@
+package com.wix.reactnativenotifications.app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "NotificationsExampleApp";
+    }
+}
---
+++ scripts/test-unit.js
@@ -0,0 +1,52 @@
+const _ = require('lodash');
+const exec = require('shell-utils').exec;
+
+const android = _.includes(process.argv, '--android');
+const release = _.includes(process.argv, '--release');
+
+function run() {
+  if (android) {
+    runAndroidUnitTests();
+  } else {
+    runIosUnitTests();
+  }
+}
+
+function runAndroidUnitTests() {
+  const conf = release ? 'testReactNative60ReleaseUnitTest' : 'testReactNative60DebugUnitTest';
+  if (android && process.env.JENKINS_CI) {
+    const sdkmanager = '/usr/local/share/android-sdk/tools/bin/sdkmanager';
+    exec.execSync(`yes | ${sdkmanager} --licenses`);
+    // exec.execSync(`echo y | ${sdkmanager} --update && echo y | ${sdkmanager} --licenses`);
+  }
+  exec.execSync(`cd lib/android && ./gradlew ${conf}`);
+}
+
+function runIosUnitTests() {
+  const conf = release ? `Release` : `Debug`;
+  exec.execSync('npm run pod-install');
+  exec.execSync(`cd ./example/ios &&
+            RCT_NO_LAUNCH_PACKAGER=true
+            xcodebuild build build-for-testing
+            -scheme "NotificationsExampleApp"
+            -workspace NotificationsExampleApp.xcworkspace
+            -sdk iphonesimulator
+            -configuration ${conf}
+            -derivedDataPath ./example/ios/DerivedData/NotificationsExampleApp
+            -quiet
+            -UseModernBuildSystem=NO
+            ONLY_ACTIVE_ARCH=YES`);
+
+  exec.execSync(`cd ./example/ios &&
+            RCT_NO_LAUNCH_PACKAGER=true
+            xcodebuild test-without-building
+            -scheme "NotificationsExampleApp"
+            -workspace NotificationsExampleApp.xcworkspace
+            -sdk iphonesimulator
+            -configuration ${conf}
+            -destination 'platform=iOS Simulator,name=iPhone 11'
+            -derivedDataPath ./example/ios/DerivedData/NotificationsExampleApp
+            ONLY_ACTIVE_ARCH=YES`);
+}
+
+run();
